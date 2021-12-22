3eac9edd75b8308fd38cc23b9d9dc6caa3c5b595
Triple the Boba ! <coconuttt1010@gmail.com>
2021-12-22 13:57:12 -0800

    remove nested git

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+/**
+ * For words count in tweet input box
+ * using jQuery and and selector to register an event handler
+ * to the <textarea> in .new-tweet
+ */
+
+/**
+ * function to console.log in DOM's console to
+ * check if script is loaded
+ * -> run a callback when the DOM is ready to be manipulated with jQuery
+ * -> ensure access HTML elements that the browser has loaded
+ */
+$(document).ready(function() {
+  console.log('Script composer-char-counter.js is successfully loaded');
+});
