815c6e30a3c9d55199ed80a9abd44b00e7856fe7
Triple the Boba ! <coconuttt1010@gmail.com>
2022-02-02 17:09:21 -0800

    update charCounter function and its style

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -12,4 +12,39 @@
  */
 $(document).ready(function() {
   console.log('Script composer-char-counter.js is successfully loaded');
+  
+  const MAX_CHARS = 140;
+  
+  // using jQuery
+  $("#tweet-text").on('input', characterCounter);
+  
+  function characterCounter() {
+    // this = <textarea> input
+    let currentChars = $(this).val().length;
+    let charsLeft = MAX_CHARS - currentChars;
+    $(".counter").text(charsLeft).removeClass("warning");
+    if (charsLeft < 0) {
+      $(".counter").addClass("warning");
+    }
+  };
 });
+
+
+// using addEventListener
+// const tweet_text = document.getElementById("tweet-text");
+// tweet_text.addEventListener('input', characterCounter);  // if use characterCounter(), cb will be run before input
+
+// function characterCounter() {
+//   // console.log('Form has input!');
+//   let currentChars = tweet_text.value.length;
+//   let charsLeft = MAX_CHARS - currentChars;
+//   // display the character counter link to index.html
+//   document.getElementById("counter").textContent = charsLeft;
+// };
+
+// $('#tweet-text').on('click', function() {
+//   console.log(this);
+// })
+// $('#tweet-text').on('click', () => {
+//   console.log(this);
+// }) // doesn't specific since it refers to a whole windown due to () =>
